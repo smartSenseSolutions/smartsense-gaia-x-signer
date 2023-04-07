f2223ae7ed3149dd64cfb184555b4c7bcd12ce5e
Ronak Thacker <ronak.thacker@smartsensesolutions.com>
2023-04-07 17:35:38 +0530

    swagger server url updated

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,9 @@ const app: Express = express()
 const port = process.env.PORT
 
 import * as swaggerDocument from './swagger.json'
+
+swaggerDocument.servers[0].url = process.env.HOST || `http://localhost:${port}`
+
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 // body-parser
 app.use(bodyParser.json({ limit: '50mb', type: 'application/json' }))
